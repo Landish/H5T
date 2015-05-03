750f7d6dd27ea00d4becb874484556b62b10619d
Lado Lomidze <lado.lomidze@gmail.com>
2015-05-03 15:35:21 +0200

    v

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,10 @@
-var gulp = require('gulp'),
-    path = require('path'),
-    plugins = require('gulp-load-plugins')();
+var elixir = require('laravel-elixir');
+require('laravel-elixir-sass-compass');
 
-// Source Paths
-var src = {
-    "js" : "src/js/**/*.js",
-    "sass" : "src/sass/*.scss"
-};
+elixir(function(mix) {
 
-// Destination Paths
-var dest = {
-    "css" : "../assets/css",
-    "images" : "../assets/images",
-    "js" : "assets/js",
-    "js_file" : "application.js"
-};
-
-// Compass
-gulp.task('css', function() {
-    return gulp.src(src.sass)
-        .pipe(plugins.compass({
-            project: path.join(__dirname, 'src'),
-            style: "compressed",
-            css: dest.css,
-            generated_images_path: dest.images
-        }))
-        .on('error', function(error) {
-            console.log(error);
-            this.emit('end');
-        })
-        .pipe(gulp.dest(dest.css));
+    mix.compass('app.scss', null, {
+        image: 'dist/images',
+        sourcemap: true
+    });
 });
-
-// Concatenate and Minify Javascript Files
-gulp.task('js', function() {
-   return gulp.src(src.js)
-       .pipe(plugins.concat(dest.js_file))
-       .pipe(plugins.uglify())
-       .pipe(gulp.dest(dest.js));
-});
-
-// Watch for Javascript files only
-gulp.task('watch:js', function() {
-    gulp.watch(src.js, ['js'])
-});
-
-// Watch for CSS files only
-gulp.task('watch:css', function() {
-    gulp.watch(src.sass, ['css'])
-});
-
-// Watch for CSS and Javascript files
-gulp.task('watch', ['watch:css', 'watch:js']);
-
-// Watch for CSS and Javascript files via default task
-gulp.task('default', ['watch']);
